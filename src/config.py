e278215efe20e5e8d6ee2fdfafd973777e9a9595
jaka2005 <jakalogin1808@gmail.com>
2022-12-16 15:44:59 +0400

    debug

--- src/config.py
+++ src/config.py
@@ -12,37 +12,37 @@ class ConfigReader():
         config = configparser.ConfigParser()
         config.read(config_path)
         
-        self.__dir__["is_primary_start"] = config.getboolean(section, "is_primary_start")
+        self.__dict__["is_primary_start"] = config.getboolean(section, "is_primary_start")
         if self.is_primary_start:
-            self.__dir__["last_time"] = -1
-            self.__dir__["voice"] = engine.getProperty("voice")
+            self.__dict__["last_time"] = -1
+            self.__dict__["voice"] = engine.getProperty("voice")
             config.set(section, "voice", self.voice)
-            self.__dir__["rate"] = engine.getProperty("rate")
+            self.__dict__["rate"] = engine.getProperty("rate")
             config.set(section, "rate", self.rate)
-            self.__dir__["volume"] = engine.getProperty("volume")
+            self.__dict__["volume"] = engine.getProperty("volume")
             config.set(section, "volume", self.volume)
         else:
-            self.__dir__["last_time"] = config.getint(section, "last_time")
-            self.__dir__["voice"] = config.get(section, "voice")
-            self.__dir__["rate"] = config.get(section, "rate")
-            self.__dir__["volume"] = config.get(section, "volume")
+            self.__dict__["last_time"] = config.getint(section, "last_time")
+            self.__dict__["voice"] = config.get(section, "voice")
+            self.__dict__["rate"] = config.get(section, "rate")
+            self.__dict__["volume"] = config.get(section, "volume")
 
-        self.__dir__["night"] = DayTimeConfig(config, "NIGHT")
-        self.__dir__["morning"] = DayTimeConfig(config, "MORNING")
-        self.__dir__["monday"] = DayTimeConfig(config, "MONDAY")
-        self.__dir__["evening"] = DayTimeConfig(config, "EVENING")
+        self.__dict__["night"] = DayTimeConfig(config, "NIGHT")
+        self.__dict__["morning"] = DayTimeConfig(config, "MORNING")
+        self.__dict__["monday"] = DayTimeConfig(config, "MONDAY")
+        self.__dict__["evening"] = DayTimeConfig(config, "EVENING")
 
-        self.__dir__["_section"] = section
-        self.__dir__["_config "]= config
+        self.__dict__["_section"] = section
+        self.__dict__["_config "]= config
 
     def __setattr__(self, __name: str, __value: Any) -> None:
         if __name in ("evening", "monday", "morning", "night", "_section", "_config"):
             raise AttributeError(f"{self.__class__}.{__name} cannot be set.")
         
-        attr_type = type(self.__dir__[__name])
+        attr_type = type(self.__dict__[__name])
         if type(__value) == attr_type:
             self._config.set(self._section, __name, __value)
-            self.__dir__[__name] == __value
+            self.__dict__[__name] == __value
         else:
             raise TypeError(f"{self.__class__}.{__name} must be of type {attr_type}")
 
